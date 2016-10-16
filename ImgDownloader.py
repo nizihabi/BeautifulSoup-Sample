c62ac8a03e4c4d6b187521177f4b6321e5379a1a
Shaoji Cai <nizihabi@qq.com>
2016-10-16 15:23:37 +0800

    新增用Selenium模拟浏览器对动态页面进行爬取的样例

--- ImgDownloader.py
+++ ImgDownloader.py
@@ -31,8 +31,21 @@ def saveImg(dir,imgLinkList):
         except:
             continue
 
-htmlSoup = getSoup("http://tieba.baidu.com/p/4809230615")
-imgs = getFileList(htmlSoup,**{"class":"BDE_Image"})
-saveImg("./imgData",imgs)
+#htmlSoup = getSoup("http://tieba.baidu.com/p/4809230615")
+#imgs = getFileList(htmlSoup,**{"class":"BDE_Image"})
+#saveImg("./imgData",imgs)
 
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.933.400 QQBrowser/9.4.8699.400",
+    "Cookie":"cuid=1097120620; pac_uid=1_1493982441; tvfe_boss_uuid=a681e78506f4f48a; RK=2nEWCbNjO1; pgv_pvi=3598561280; o_cookie=1493982441; ptcz=9258c903a08880c4ce0d71f4de8eb68e054f516954968aa6a6204a11d418bf13; pt2gguin=o1493982441; theme=dark; roastState=1; pgv_si=s8046535680; pc_userinfo_cookie=; ts_refer=hanhuazu.cc/cartoon/post; readRecord=%5B%5B505430%2C%22%E6%B5%B7%E8%B4%BC%E7%8E%8B%2F%E8%88%AA%E6%B5%B7%E7%8E%8B%22%2C828%2C%22%E7%AC%AC811%E8%AF%9D%20ROKO%22%2C811%5D%5D; readLastRecord=%5B%5D; pgv_info=ssid=s6644996197; pgv_pvid=4115647938; ts_uid=9386960475",
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Connection":"keep-alive",
+    "Host":"ac.qq.com",
+    "Accept-Encoding":"gzip, deflate, sdch"
+    }
+
+opener = request.build_opener()
+opener.addheaders = [header]
+data = opener.open("http://ac.qq.com/ComicView/index/id/505430/cid/829").read()
+print(data)
 
---
+++ SeleniumSample.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+import requests
+import os
+from bs4 import BeautifulSoup
+import time
+
+class ChromeBrowser(object):
+    def __init__(self,chromedriver):
+        self.__browser = webdriver.Chrome(chromedriver)
+
+    def getSoup(self,url):
+        self.__browser.get(url)
+        for i in range(0,28000,800):   #采用for循环模拟鼠标滚轮下拉，并设置一定时间让图片缓存出来
+            js = "window.scrollTo(0,"+str(i)+")"
+            self.__browser.execute_script(js)
+            time.sleep(0.25)
+        soup = BeautifulSoup(self.__browser.page_source)
+        self.__browser.quit()
+        return soup
+
+    """
+    获取图片链接
+    """
+    def getFileList(self,soup):
+        return [img["src"]  for img in soup.find_all("img",{"class":"loaded"}) ]
+
+
+    """
+    保存图片到指定目录
+    """
+    def saveImage(self,imgUrl, dir, imgname):
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        imgname = dir + imgname
+        response = requests.get(imgUrl, stream=True)
+        image = response.content
+        print("保存文件" + imgname)
+        try:
+            with open(imgname, "wb") as jpg:
+                jpg.write(image)
+        except IOError:
+            print("IO Error\n")
+
+
+
+
+if __name__ == "__main__":
+    chromedriver = r"./dependence/chromedriver" #selenium的驱动所在
+    CONSTURL = "http://ac.qq.com/ComicView/index/id/505430/cid/" #url前面不变的部分
+    startIndex = 810 #开始的章节
+    endIndex = 843  #结束的章节
+    offset = 17 #章节和真正url序号之间的差值
+    for index in range(startIndex,endIndex):  #开始每一章节的爬虫
+        mybrowser = ChromeBrowser(chromedriver) #生成一个Chrome浏览器
+        dir = "./ImgData/" + str(index) + "/" #保存的目录
+        link = CONSTURL+str(index + offset) #组合url
+        page = mybrowser.getSoup(link) #获取最后经过js渲染之后的真正的页面内容
+        imgs = mybrowser.getFileList(page) #获取每一页的图片
+        index = 0
+        for img in imgs:
+            imgName = str(index) + ".jpg"
+            mybrowser.saveImage(img, dir, imgName)
+            index += 1
+
+    print("已经下载完成")
