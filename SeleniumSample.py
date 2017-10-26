828f71f0c1d31ef598bf98bb65a70a1b2089c476
Shaoji Cai <nizihabi@qq.com>
2017-10-26 17:37:14 +0800

    提交

--- SeleniumSample.py
+++ SeleniumSample.py
@@ -3,20 +3,24 @@ import requests
 import os
 from bs4 import BeautifulSoup
 import time
+import threading
 
 class ChromeBrowser(object):
     def __init__(self,chromedriver):
         self.__browser = webdriver.Chrome(chromedriver)
 
     def getSoup(self,url):
-        self.__browser.get(url)
-        for i in range(0,28000,800):   #采用for循环模拟鼠标滚轮下拉，并设置一定时间让图片缓存出来
-            js = "window.scrollTo(0,"+str(i)+")"
-            self.__browser.execute_script(js)
-            time.sleep(0.25)
-        soup = BeautifulSoup(self.__browser.page_source)
-        self.__browser.quit()
-        return soup
+        try:
+            self.__browser.get(url)
+            for i in range(0,28000,800):   #采用for循环模拟鼠标滚轮下拉，并设置一定时间让图片缓存出来
+                js = "window.scrollTo(0,"+str(i)+")"
+                self.__browser.execute_script(js)
+                time.sleep(0.25)
+            soup = BeautifulSoup(self.__browser.page_source)
+            self.__browser.quit()
+            return soup
+        except:
+            return None
 
     """
     获取图片链接
@@ -41,25 +45,29 @@ class ChromeBrowser(object):
         except IOError:
             print("IO Error\n")
 
-
+    def downLoadThread(self,dir,imgs):
+        index = 0
+        for img in imgs:
+            imgName = str(index) + ".jpg"
+            self.saveImage(img, dir, imgName)
+            index += 1
 
 
 if __name__ == "__main__":
     chromedriver = r"./dependence/chromedriver" #selenium的驱动所在
     CONSTURL = "http://ac.qq.com/ComicView/index/id/505430/cid/" #url前面不变的部分
     startIndex = 810 #开始的章节
-    endIndex = 843  #结束的章节
+    endIndex = 844  #结束的章节
     offset = 17 #章节和真正url序号之间的差值
     for index in range(startIndex,endIndex):  #开始每一章节的爬虫
         mybrowser = ChromeBrowser(chromedriver) #生成一个Chrome浏览器
-        dir = "./ImgData/" + str(index) + "/" #保存的目录
+        dir = "./OnePieceData/" + str(index) + "/" #保存的目录
         link = CONSTURL+str(index + offset) #组合url
         page = mybrowser.getSoup(link) #获取最后经过js渲染之后的真正的页面内容
+        if page == None:
+            continue
         imgs = mybrowser.getFileList(page) #获取每一页的图片
-        index = 0
-        for img in imgs:
-            imgName = str(index) + ".jpg"
-            mybrowser.saveImage(img, dir, imgName)
-            index += 1
+        t = threading.Thread(target=mybrowser.downLoadThread, args=( dir,imgs))
+        t.start()
 
     print("已经下载完成")
